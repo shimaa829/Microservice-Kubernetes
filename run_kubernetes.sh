be9dce251ffbe70adb31425220818d7a2529943c
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-08-12 20:25:57 +0200

    add output of ./run_kubernetes.sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,14 +4,14 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=shimaa96/my-python-ap
+dockerpath=shimaa96/my-python-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run flaskskearlnapp\
+kubectl run flaskapp\
     --generator=run-pod/v1\
     --image=$dockerpath\
-    --port=80 --labels app=flaskskearlnapp
+    --port=80 --labels app=flaskapp
 
 # Step 3:
 # List kubernetes pods
@@ -19,4 +19,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward flaskskearlnapp 8000:80
+kubectl port-forward flaskapp 8000:80
