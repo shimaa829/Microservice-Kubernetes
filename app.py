d6d17e1ed2eaeae9cf071a2028e7b15e82908588
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-08-12 21:17:01 +0200

    modify app.py file

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    
-    LOG.info(f"Scaling Payload: \n{payload}")
+
+    # LOG.info(f"Scaling Payload: \n{payload}")
+    LOG.info("Scaling Payload: %s\n", payload)
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
